356ab9a6e3d2c4a9d49aa08f62f1870bce464408
Joeri <ure@on3ure.be>
2026-01-28 22:23:34 +0100

    4g base

---
+++ install-4gmodule.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+run() {
+  exec=$1
+  printf "\x1b[38;5;104m --> ${exec}\x1b[39m\n"
+  eval ${exec}
+}
+
+say () {
+  say=$1
+  printf "\x1b[38;5;220m${say}\x1b[38;5;255m\n"
+}
+
+
+say "Installing Prerequisites"
+run "python3 -m pip install pyserial"
+

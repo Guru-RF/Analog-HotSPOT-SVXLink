3ff1d8ac5c58dda123bf9e937045e79ebf395b3d
Joeri Van Dooren <ure@moreorless.be>
2026-03-06 19:49:13 +0100

    add oled install script

---
+++ install-oled.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+run() {
+	exec=$1
+	printf "\x1b[38;5;104m --> ${exec}\x1b[39m\n"
+	eval ${exec}
+}
+
+say() {
+	say=$1
+	printf "\x1b[38;5;220m${say}\x1b[38;5;255m\n"
+}
+
+say "Installing Prerequisites"
+run "pip3 install luma.oled"
+run "apt install -y fonts-terminus fonts-terminus-otb fonts-dejavu fonts-noto-mono fonts-spleen"
+
+say "Installing SVXLink OLED Service"
+run "cp svxlink-oled.service /etc/systemd/system/svxlink-oled.service"
+run "systemctl enable svxlink-oled.service"
